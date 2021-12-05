60c7552e997e62032fe700ec0e55b78c25823e62
juansantosgomez <juansantosgomez@protonmail.com>
2021-12-05 15:08:11 +0800

    Model Account created
    - The model 'Account' is created containing the following fields:
    	- **site** - the name of the site or service provider to which
    	  the account is related to
    	- **url** - (optional) the url of the site provided
    	- **description** - (optional) short description
    	- **password** - the password to be stored
    	- **username** - (optional) the username to be stored
    	- **email** - (optional) the email used for the account
    	- **user** - the user associated with this account
    	- metadata is included for **created** and **updated**

--- passes/models.py
+++ passes/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 from django.db.models.fields.related import ForeignKey
-
+from django.contrib.auth.models import User
 # Create your models here.
 
+class Account(models.Model):
+    #data
+    site = models.CharField(max_length=50,blank=False,null=False)
+    url = models.URLField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    password = models.CharField(max_length=120,null=False, blank=False)
+    username = models.CharField(max_length=120,null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    user = ForeignKey(User,on_delete=models.CASCADE)
 
+
+    #metadata
+    created = models.DateTimeField(auto_created=True, auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
