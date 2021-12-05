b60c539e069e70989f762364bdecdf895fd6b4af
juansantosgomez <juansantosgomez@protonmail.com>
2021-12-05 16:06:23 +0800

    Account Viewset initialized in views.py
    - Account Viewset made with ModelViewset but modified to have a queryset
      thats restricted to Accounts associated with the logged user

--- passes/views.py
+++ passes/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import permissions 
+from passes.models import Account
+from passes.serializers import AccountSerializer
 # Create your views here.
+
+
+class AccountViewSet(ModelViewSet):
+
+    serializer_class = AccountSerializer
+    permission_classes = [permissions.IsAuthenticated,
+                          permissions.IsOwner]
+
+    # This sets the queryset to be all the accounts associated with the currently logged in user
+    def get_queryset(self):
+        queryset = Account.objects.filter(user=self.request.user)
+        return queryset 
+
