eb421b45f601715f12ce9123e3ea520c6c294e41
juansantosgomez <juansantosgomez@protonmail.com>
2021-12-11 21:48:38 +0800

    cors headers

--- Backend/passwords/settings.py
+++ Backend/passwords/settings.py
@@ -42,12 +42,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    #middleware
+    'corsheaders',
+    'rest_framework',
+
     #User apps
     'passes',
-    'rest_framework'
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'passwords.urls'
@@ -131,3 +137,18 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+DEFAULT_AUTHENTICATION_CLASSES = [ 'rest_framework.authentication.BasicAuthentication', 'rest_framework.authentication.SessionAuthentication', ]
+
+CORS_ALLOWED_ORIGINS = [
+        'http://127.0.0.1:3000',
+        'http://localhost:3000',
+
+        
+    ]
+CSRF_TRUSTED_ORIGINS = [
+        'http://127.0.0.1:3000',
+        'http://localhost:3000',
+
+    ]
